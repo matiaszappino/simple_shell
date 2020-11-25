4b4a8e86def7871c4ff03c9f0fd80125f3aceeb3
martinmsaavedra <martin@bildung.uy>
2020-11-25 21:27:29 +0000

    split path fix

--- split_path.c
+++ split_path.c
@@ -9,24 +9,27 @@ char *split_path(char *path, char **args)
 {
 	char *folder = NULL;
 	struct stat fileStat;
-
-	char *command = malloc(sizeof(char) * _strlen(args[0]) + _strlen(path));
-
-	if (!command)
-		return (NULL);
+	char *command;
 
 	folder = strtok(path, ":");
 	while (folder != NULL)
 	{
-		command[0] = '\0';
+		command = malloc(sizeof(char) * _strlen(args[0]) + _strlen(path));
+		if (!command)
+			return (NULL);
+		/*command[0] = '\0';*/
 		command = _strcat(command, folder);
 		command = _strcat(command, "/");
 		command = _strcat(command, args[0]);
 		if (stat(command, &fileStat) == 0)
-			return (command);
+			{
+				/*free(folder);*/
+				return (command);
+			}
 		folder = strtok(NULL, ":");
+		free(command);
 	}
-	free(command);
+	/*free(command);*/
 	free(folder);
 	return (NULL);
 }
